be2e698a34c32daba4d804027674d464e855583a
Aazim Sherazi <aazimsherazi3210@gmail.com>
2025-02-04 02:13:42 +0500

    scrollbar fixes

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -12,7 +12,7 @@ const DisplayHome = () => {
       <Navbar/>
       <div className='mb-4 '>
         <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
-        <div className='flex overflow-auto no-horizontal-scroll'>
+        <div className='flex overflow-auto scrollbar-hide'>
        {albumsData.map((item, index)=>(<AlbumItem key={index} name={item.name} image={item.image} desc={item.desc} id={item.id} />))}
         </div>
       </div>
